841ff475505c1051d51e33776637580154cdfb31
Satyam <146202467+satyamkittu91@users.noreply.github.com>
2024-12-29 06:24:13 +0530

    Functions

---
+++ Basics/Functions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void new_function() { // return_type Function_name(parameters)
+                        // if function doesn't return anything than "void" is used.
+    printf("Hello, World!\n");
+}
+
+int main(){
+    new_function();
+}
