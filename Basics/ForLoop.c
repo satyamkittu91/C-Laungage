b36c0ce858bd5f1180ec5b9a71e3b9538734c956
Satyam <146202467+satyamkittu91@users.noreply.github.com>
2024-12-29 21:36:03 +0530

    For Loop

---
+++ Basics/ForLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    // for loop = repeats a section of code a limited amount of times
+    /*for (initialization; condition; increment/decrement) {
+    // Code to be executed repeatedly
+        }*/
+
+    for(int i = 1; i <=10; i++){
+        printf("%d. test\n", i); // prints "test" 10 times
+    }
+    return 0;
+}
